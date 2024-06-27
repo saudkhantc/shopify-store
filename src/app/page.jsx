8ca123e0ba832c8f3664fcb32fd0017bd3f4e037
saudkhan <saudkhantc@gmail.com>
2024-06-27 16:47:35 +0500

    update with design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import { ProductCard } from "../components/product-card";
 export default async function Home() {
   const products = await getAllProducts();
   return (
-    <div className="flex gap-3">
+    <div className="flex flex-wrap justify-center gap-3">
       {products.length > 0 && products?.map((item) => {
         return <ProductCard node={item} key={item.id} />;
       })}
--- src/components/product-card.jsx
+++ src/components/product-card.jsx
@@ -1,7 +1,7 @@
 export const ProductCard = ({node}) => {
   const {title,id}=node
   return (
-    <div className="flex flex-col  items-center p-5 w-full border rounded-md mt-5">
+    <div className="flex flex-col  items-center p-5 max-w-fit border rounded-md mt-5">
         <h1>Product Id : {id}</h1>
         <h1>Product Title : {title}</h1>
 
